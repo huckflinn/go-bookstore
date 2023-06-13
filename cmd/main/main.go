09276e66a67778e848b046719d17883d6e4851b3
Nick Flinn <nicholas@flinn.net>
2023-06-13 10:40:50 -0400

    Implement basic server functionality

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/huckflinn/go-bookstore/pkg/routes"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010"))
 }
